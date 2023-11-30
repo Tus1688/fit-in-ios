61cc634fdf3aad1b927db753c96d228a11a783a0
GamalielFeliksFeranta <gferanta@student.ciputra.ac.id>
2023-11-30 13:46:26 +0700

    Initial Commit

---
+++ SleepTracker/SleepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTrackerApp.swift
+//  SleepTracker
+//
+//  Created by MacBook Pro on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SleepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
